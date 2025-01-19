9976c5c4faba3e133b082517e3d97b5e1f3956f6
BANDE Abdoul Aziz <abdoulaziz.bande@kreezus.com>
2025-01-19 23:22:12 +0000

    add layout

---
+++ src/screens/Layout.tsx
@@ -0,0 +1,41 @@
+import { Suspense, lazy } from "react";
+import { useMediaQuery } from "react-responsive";
+import { Outlet } from "react-router-dom";
+import { ThemeProvider } from "../providers/theme";
+import { Header } from "../components/Header";
+import { TabBar } from "../components/TabBar";
+import { Footer } from "../components/Footer";
+import { mediaBreakpoints } from "../common";
+
+const LeftBar = lazy(() => import("../components/LeftBar/LeftBar"));
+const SocialNetworks = lazy(() => import("./SocialNetworks"));
+
+export const Layout = () => {
+  const mobileAndTablet = useMediaQuery({
+    query: `(max-width: ${mediaBreakpoints.xl}px)`,
+  });
+
+  return (
+    <ThemeProvider>
+      <div className="flex h-[100svh] w-full flex-col">
+        <Header mobileAndTablet={mobileAndTablet} />
+
+        <section className="flex flex-col-reverse w-full overflow-hidden h-content xl:flex-row">
+          {!mobileAndTablet && <SocialNetworks />}
+          <LeftBar />
+          <section className="w-full h-full pb-2 overflow-hidden main-window bg-bgColor">
+            <TabBar mobileAndTablet={mobileAndTablet} />
+
+            <main className="px-4 pt-4 page-wrapper sm:px-8">
+              <Suspense fallback={<div></div>}>
+                <Outlet />
+              </Suspense>
+            </main>
+          </section>
+        </section>
+
+        <Footer mobileAndTablet={mobileAndTablet} />
+      </div>
+    </ThemeProvider>
+  );
+};
